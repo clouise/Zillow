f7aa43345d781b09e463f41dada4f4890efea50b
clouise <CarmelloReyes@tutanota.com>
2017-08-25 11:49:15 -0400

    Carmen 5 results

--- Zillow.R
+++ Zillow.R
@@ -1,6 +1,4 @@
-library(reshape2)
 library(data.table)
-library(ggplot2)
 library(caret)
 library(xgboost)
 library(Metrics)
@@ -11,7 +9,9 @@ prop <- fread('/Users/carmenlouise/Documents/Zillow/properties_2016.csv')
 t <- fread('/Users/carmenlouise/Documents/Zillow/train_2016_v2.csv')
 #sample <- fread("/Users/carmenlouise/Documents/Zillow/sample_submission.csv", header = TRUE)
 
-t$month <- as.numeric(substr(t$transactiondate,6,7))
+#t$month <- as.numeric(substr(t$transactiondate,6,7))
+#t %>% group_by(month) %>% dplyr::summarise(logerror = mean(logerror))
+
 # fit <- lm(logerror ~ as.factor(month) +0, data= t)
 # summary(fit)
 # anova(fit)
@@ -21,21 +21,16 @@ t$month <- as.numeric(substr(t$transactiondate,6,7))
 # ggplot(t, aes(x = as.factor(month), y = logerror)) +
 #   geom_boxplot()
 
-t2 <- filter(t, logerror > -.4 | logerror < .4)
+t2 <- filter(t, logerror > -.4 | logerror < .42)
 rm(t);gc()
 
-prop$airconditioningtypeid[is.na(prop$airconditioningtypeid)] <- 0
-prop$architecturalstyletypeid[is.na(prop$architecturalstyletypeid)] <- 0
-prop$buildingclasstypeid[is.na(prop$buildingclasstypeid)] <- 0
-prop$buildingqualitytypeid[is.na(prop$buildingqualitytypeid)] <- 0
-prop$propertylandusetypeid[is.na(prop$propertylandusetypeid)] <- 0
-prop$storytypeid[is.na(prop$storytypeid)] <- 0
-prop$typeconstructiontypeid[is.na(prop$typeconstructiontypeid)] <- 0
+# imp_values <- preProcess(prop,
+#                          method = c("medianImpute") # May not be needed
+# )
+# prop <- predict(imp_values, prop)
 
-imp_values <- preProcess(prop,
-                         method = c("medianImpute") # May not be needed
-)
-prop <- predict(imp_values, prop)
+names <- prop[, -which(colMeans(is.na(prop)) >= 0.8)]
+prop <- select(prop,-one_of(names(names)))
 
 prop$longmean <- mean((prop$longitude), na.rm=TRUE)/1e6
 prop$latmean <- mean((prop$latitude), na.rm=TRUE)/1e6
@@ -59,6 +54,8 @@ for(i in cidx){
   prop[[i]] <- as.integer(factor(prop[[i]]))
 }
 
+prop[is.na(prop)] <- 0
+
 train <- merge(prop, t2, by="parcelid", all.y=TRUE)
 
 set.seed(181)
@@ -79,20 +76,20 @@ control <- trainControl(method = "cv",
 )
 
 xgb_grid_1 = expand.grid(
-  nrounds = c(200,300,500),
+  nrounds = c(500),
   subsample = c(.75,.8,.85),
-  max_depth = c(4,5,6,7,8),
-  eta = c(.06,.05,.04,.03,.02),
+  max_depth = c(5,6,8),
+  eta = c(.04,.035,.03,.025),
   colsample_bytree=c(.4,.5),
-  min_child_weight=c(1,4),
+  min_child_weight=c(4),
   gamma=c(.5)
 )
 
 xgb_grid_2 = expand.grid(
-  nrounds = c(300),
-  subsample = c(.75),
-  max_depth = c(4),
-  eta = c(.04),
+  nrounds = c(500),
+  subsample = c(.8),
+  max_depth = c(6),
+  eta = c(.033),
   colsample_bytree=c(.5),
   min_child_weight=c(4),
   gamma=c(.5)
@@ -101,7 +98,10 @@ xgb_grid_2 = expand.grid(
 traindata <- as.matrix(select(train,-censustractandblock,-transactiondate,-logerror,-parcelid))
 target <- train[,logerror]
 
-rm(t2);gc()
+parcelid <- select(prop,parcelid)
+prop2 <- select(prop,-censustractandblock,-parcelid)
+
+rm(t2);rm(prop);gc()
 cb <- train(y=target,
             x=traindata, 
             preProcess = NULL,
@@ -115,16 +115,34 @@ cb <- train(y=target,
 
 # nrounds = 200, max_depth = 4, eta = 0.04, gamma = 0.5, colsample_bytree = 0.5, min_child_weight = 4 and subsample = 0.75
 print(cb)
-prop2 <- select(prop,-censustractandblock,-parcelid)
 
-rm(prop);gc()
 
-prop2$month <- 10
-cb_prediction2 <- predict(cb, prop2)
-cb_prediction2
-mean(cb_prediction2)
-predictions1 <- round(as.vector(cb_prediction1), 4)
+#prop2$month <- 12
+cb_prediction <- predict(cb, prop2)
+mean(cb_prediction)
+predictions <- round(as.vector(cb_prediction), 4)
 
-result <- data.frame(cbind(prop$parcelid, predictions1, predictions2, predictions3, predictions1, predictions2, prediction3))
+result <- data.frame(cbind(parcelid$parcelid, predictions, predictions, predictions, predictions, predictions, predictions))
 colnames(result) <- c("parcelid","201610","201611","201612","201710","201711","201712")
-write.csv(result, file = "Carmen_4.csv", row.names = FALSE)
+
+t$month <- as.numeric(substr(t$transactiondate,6,7))
+t %>% group_by(month) %>% dplyr::summarise(logerror = mean(logerror))
+
+result$`201610` <- result$`201610`*.95+.09*0.016341632
+result$`201611` <- result$`201611`*.95+.09*0.014510131
+result$`201612` <- result$`201612`*.95+.09*0.019122312
+
+write.csv(result, file = "Carmen_5.csv", row.names = FALSE)
+
+
+
+
+
+#######################################
+df <- fread("carmy.csv")
+df <- round(df,4)
+df$`201610` <- df$`201610`*.95+.09*0.016341632
+df$`201611` <- df$`201611`*.95+.09*0.014510131
+df$`201612` <- df$`201612`*.95+.09*0.019122312
+mean(df$`201610`)
+write.csv(df,"Revision.csv",row.names = FALSE)
